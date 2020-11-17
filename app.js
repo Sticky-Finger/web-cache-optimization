6026f8de1535b5beb1f29280e115870ea288c774
niepeng230 <niepeng230@pingan.com.cn>
2020-11-18 02:10:47 +0800

    feat: response header添加Last-Modified

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ let server = http.createServer(function(request, response) {
     ext = ext ? ext.slice(1) : 'unknown';
     const contentType = mime[ext] || 'text/plain';
 
+    // 设置强缓存
     if (ext.match(config.Expires.fileMatch)) {
       const expires = new Date();
       expires.setTime(expires.getTime() + config.Expires.maxAge * 1000);
@@ -21,31 +22,51 @@ let server = http.createServer(function(request, response) {
       response.setHeader('Cache-Control', 'max-age=' + config.Expires.maxAge);
     }
 
-    fs.exists(realPath, function (exists) {
-      if (!exists) {
+    // 设置协商缓存
+    fs.stat(realPath, function (err, stat) {
+      if (err) {
         response.writeHead(404, {
           'Content-Type': 'text/plain',
         });
 
         response.write('This request URL ' + pathname + ' was not found on this server');
         response.end();
-      } else {
-        fs.readFile(realPath, 'binary', function (err, file) {
-          if (err) {
-            response.writeHead(500, {
-              'Content-Type': 'text/plain',
-            });
-            response.end();
-          } else {
-            response.writeHead(200, {
-              'Content-Type': contentType,
-            });
-            response.write(file, 'binary');
-            response.end();
-          }
-        });
+        return;
       }
+
+      // console.log('realPath', realPath);
+      // console.log('stat', stat);
+      const lastModified = stat.mtime.toUTCString();
+      response.setHeader('Last-Modified', lastModified);
+
+      fs.exists(realPath, function (exists) {
+        if (!exists) {
+          response.writeHead(404, {
+            'Content-Type': 'text/plain',
+          });
+  
+          response.write('This request URL ' + pathname + ' was not found on this server');
+          response.end();
+        } else {
+          fs.readFile(realPath, 'binary', function (err, file) {
+            if (err) {
+              response.writeHead(500, {
+                'Content-Type': 'text/plain',
+              });
+              response.end();
+            } else {
+              response.writeHead(200, {
+                'Content-Type': contentType,
+              });
+              response.write(file, 'binary');
+              response.end();
+            }
+          });
+        }
+      });
     });
+
+    
 });
 
 server.listen(PORT);
